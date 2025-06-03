5a0fd136adebc33023fb0a1dce7325b88f542909
LushoCallapa <luiscallapa2004@gmail.com>
2025-06-03 10:45:14 -0400

    Matches fuctions Solutions

--- postgres-valorant/functions/get_player_win_rate.sql
+++ postgres-valorant/functions/get_player_win_rate.sql
@@ -8,18 +8,26 @@ RETURNS TABLE (
 AS $$
 BEGIN
     RETURN QUERY
-    SELECT  COUNT(*)::integer as total_matches,SUM(CASE WHEN m.winning_team_id = t.id THEN 1 ELSE 0 END)::integer AS wins,
-        SUM(CASE WHEN m.losing_team_id = t.id THEN 1 ELSE 0 END)::integer AS losses,
+    SELECT 
+        COUNT(*)::integer AS total_matches,
+        SUM(CASE WHEN mr.winning_team_id = t.id THEN 1 ELSE 0 END)::integer AS wins,
+        SUM(CASE WHEN mr.losing_team_id = t.id THEN 1 ELSE 0 END)::integer AS losses,
         ROUND(
-            SUM(CASE WHEN m.winning_team_id = t.id  THEN 1 ELSE 0 END) * 100.0 / 
+            SUM(CASE WHEN mr.winning_team_id = t.id THEN 1 ELSE 0 END) * 100.0 / 
             NULLIF(COUNT(*), 0),
         2)::numeric AS win_rate
-	from players p
-	inner join teams t 
-	on t.player_1 = p.id or t.player_2 = p.id or t.player_3 = p.id or t.player_4 = p.id or t.player_5 = p.id
-	inner join matches m on m.winning_team_id = t.id or m.losing_team_id = t.id 
-	where p.id = p_player_id;
+    FROM players p
+    INNER JOIN teams t ON 
+        t.player_1 = p.id OR 
+        t.player_2 = p.id OR 
+        t.player_3 = p.id OR 
+        t.player_4 = p.id OR 
+        t.player_5 = p.id
+    INNER JOIN matches_result mr ON 
+        mr.winning_team_id = t.id OR mr.losing_team_id = t.id
+    WHERE p.id = p_player_id;
 END;
 $$ LANGUAGE plpgsql;
+
 
 SELECT * FROM get_player_win_rate(406);
