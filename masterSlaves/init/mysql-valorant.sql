d928cd4354ad44897b3251fbae2bd67e6242e094
LushoCallapa <luiscallapa2004@gmail.com>
2025-06-16 23:05:28 -0400

    Master Cache y Sharding

---
+++ masterSlaves/init/mysql-valorant.sql
@@ -0,0 +1,85 @@
+CREATE TABLE payment_methods (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    method_name VARCHAR(50) NOT NULL UNIQUE,
+    description VARCHAR(255)
+);
+
+CREATE TABLE transaction_types (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    type_code VARCHAR(30) UNIQUE NOT NULL,
+    description VARCHAR(100)
+);
+
+CREATE TABLE vp_packages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    vp_amount INT NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    bonus_vp INT DEFAULT 0
+);
+
+CREATE TABLE wallets (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL UNIQUE,
+    balance DECIMAL(15,2) DEFAULT 0
+);
+
+CREATE TABLE transactions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    transaction_date DATETIME NOT NULL,
+    amount DECIMAL(10,2) NOT NULL,
+    payment_method_id INT,
+    transaction_type_id INT,
+    status VARCHAR(20),
+    FOREIGN KEY (payment_method_id) REFERENCES payment_methods(id),
+    FOREIGN KEY (transaction_type_id) REFERENCES transaction_types(id)
+);
+
+CREATE TABLE purchases (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    transaction_id INT NOT NULL,
+    item_name VARCHAR(100) NOT NULL,
+    item_type VARCHAR(50),
+    quantity INT DEFAULT 1,
+    price DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY (transaction_id) REFERENCES transactions(id)
+);
+
+CREATE TABLE wallet_transactions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    wallet_id INT NOT NULL,
+    transaction_id INT NOT NULL,
+    vp_package_id INT NOT NULL,
+    vp_amount INT NOT NULL,
+    created_at DATETIME NOT NULL,
+    FOREIGN KEY (wallet_id) REFERENCES wallets(id),
+    FOREIGN KEY (transaction_id) REFERENCES transactions(id),
+    FOREIGN KEY (vp_package_id) REFERENCES vp_packages(id)
+);
+
+create table transaction_errors (
+    id int auto_increment primary key,
+    transaction_id int not null,
+    error_message varchar(255),
+    created_at datetime default current_timestamp,
+    FOREIGN KEY (transaction_id) REFERENCES transactions(id)
+);
+
+
+-- Particionamientos
+
+-- Particionamiento de la tabla wallet_transactions
+-- create table wallet_transactions_partitioned (
+--     id int auto_increment primary key,
+--     wallet_id int not null,
+--     transaction_id int not null,
+--     vp_package_id int not null,
+--     vp_amount int not null,
+--     created_at datetime not null
+-- )
+-- partition by range (year(created_at)) (
+--     partition p2023 values less than (2024),
+--     partition p2024 values less than (2025),
+--     partition p2025 values less than (2026)
+-- );
---
+++ sharding/init/postgres-valorant.sql
@@ -0,0 +1,114 @@
+CREATE TABLE country (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    code CHAR(2) NOT NULL UNIQUE
+);
+
+CREATE TABLE regions (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    code CHAR(2) NOT NULL UNIQUE
+);
+
+CREATE TABLE rol (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL UNIQUE
+);
+
+create table rank_levels (
+    id serial primary key,
+    rank_name varchar(20) not null unique,
+    rank_order int not null unique
+);
+CREATE TABLE players (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    rank_id int references rank_levels(id),
+    level INT,
+    country INT NOT NULL REFERENCES country(id),
+    regions INT NOT NULL REFERENCES regions(id),
+    create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE IF NOT EXISTS info_players (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    player_id INT NOT NULL UNIQUE REFERENCES players(id),
+    create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE teams (
+    id SERIAL PRIMARY KEY,
+    player_1 INT NOT NULL REFERENCES players(id),
+    player_2 INT NOT NULL REFERENCES players(id),
+    player_3 INT NOT NULL REFERENCES players(id),
+    player_4 INT NOT NULL REFERENCES players(id),
+    player_5 INT NOT NULL REFERENCES players(id)
+);
+
+CREATE TABLE maps (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE agents (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    role INT NOT NULL REFERENCES rol(id)
+);
+
+CREATE TABLE matches (
+    id SERIAL PRIMARY KEY,
+    map_id INT NOT NULL,
+    match_date DATE NOT NULL,
+    duration_minutes INT DEFAULT -1
+);
+
+CREATE TABLE matches_P (
+    id BIGSERIAL NOT NULL,
+    map_id INT NOT NULL,
+    match_date DATE NOT NULL,
+    duration_minutes INT DEFAULT -1,
+    PRIMARY KEY (id, match_date)
+) PARTITION BY RANGE (match_date);
+
+CREATE TABLE matches_result (
+    match_id INT PRIMARY KEY REFERENCES matches(id),
+    winning_team_id INT NOT NULL REFERENCES teams(id),
+    losing_team_id INT NOT NULL REFERENCES teams(id)
+);
+
+CREATE TABLE player_stats (
+    id SERIAL PRIMARY KEY,
+    player_id INT NOT NULL REFERENCES players(id),
+    match_id INT NOT NULL REFERENCES matches(id),
+    agent_id INT NOT NULL REFERENCES agents(id),
+    team_id INT NOT NULL REFERENCES teams(id),
+    kills INT,
+    deaths INT,
+    assists INT
+);
+
+CREATE TABLE player_player_stats (
+    id SERIAL PRIMARY KEY,
+    player_id INT NOT NULL REFERENCES players(id),
+    opponent_id INT NOT NULL REFERENCES players(id),
+    match_id INT NOT NULL REFERENCES matches(id),
+    kills INT,
+    deaths INT,
+    assists INT
+);
+
+CREATE TABLE matches_act0 PARTITION OF matches_P
+FOR VALUES FROM (MINVALUE) TO ('2025-01-01');
+
+CREATE TABLE matches_act1 PARTITION OF matches_P
+FOR VALUES FROM ('2025-01-01') TO ('2025-05-01');
+
+CREATE TABLE matches_act2 PARTITION OF matches_P
+FOR VALUES FROM ('2025-05-01') TO ('2025-09-01');
+
+CREATE TABLE matches_act3 PARTITION OF matches_P
+FOR VALUES FROM ('2025-09-01') TO ('2026-01-01');
